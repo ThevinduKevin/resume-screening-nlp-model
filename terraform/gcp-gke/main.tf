1fd53ed5563a10a06a9ab6e0d31e2a6ba833c9d4
Thevindu Kevin <thevinduk@subtletechs.com>
2026-02-11 06:55:20 +0530

    fix: enhance deployment scripts with improved health check logic and retry mechanism; add disk type to GKE node pool configuration

--- terraform/gcp-gke/main.tf
+++ terraform/gcp-gke/main.tf
@@ -83,6 +83,7 @@ resource "google_container_node_pool" "primary_nodes" {
   node_config {
     machine_type = var.node_machine_type
     disk_size_gb = 50
+    disk_type    = "pd-standard"
 
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
