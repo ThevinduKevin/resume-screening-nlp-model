f04b8f1636a8278b8602d83f808923e70e70aff5
Thevindu Kevin <thevinduk@subtletechs.com>
2026-02-12 20:46:22 +0530

    Add Artifact Registry to GKE terraform and fix workflow

--- terraform/gcp-gke/outputs.tf
+++ terraform/gcp-gke/outputs.tf
@@ -22,3 +22,8 @@ output "project_id" {
   description = "GCP Project ID"
   value       = var.project_id
 }
+
+output "artifact_registry_url" {
+  description = "Artifact Registry URL"
+  value       = "${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.ml_repo.repository_id}"
+}
